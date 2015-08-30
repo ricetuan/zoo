d90f06acbea077935d289bb9db87fc5bb65483dd
liang.fan <fanliang88@gmail.com>
2015-08-30 22:29:13 +0900

    fix build-cfg bugs for android build

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -6,14 +6,20 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+CPP_FILES := $(shell find $(LOCAL_PATH)/../../Classes -name *.cpp)
+
+LOCAL_SRC_FILES := hellocpp/main.cpp
+LOCAL_SRC_FILES += $(CPP_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../cocos2d/cocos/ui
 
-LOCAL_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,./prebuilt-mk)
+$(call import-module,.)
+$(call import-module,audio/android)
+$(call import-module,editor-support/cocostudio)
